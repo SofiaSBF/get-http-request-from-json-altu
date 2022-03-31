5dc82edb755a9fcc3f71b7a7b137cf8bb39eea3e
Cesar Goulart <devcesargoulart@gmail.com>
2022-03-31 00:20:45 -0300

    add listFile function

--- src/commands/get-http-request-from-json.js
+++ src/commands/get-http-request-from-json.js
@@ -2,8 +2,16 @@ const command = {
   name: 'get-http-request-from-json',
   run: async toolbox => {
     const { print } = toolbox
+    
+    try{
+      var files = toolbox.listValidJsonFile();
+      print.muted('ijoijoijjlkjlkj')
+    }catch(err){
+      print.error(err.message || 'Erro ao tentar ler os arquivos');
+      return
+    }
 
-    print.info('Welcome to your CLI')
+    
   }
 }
 
