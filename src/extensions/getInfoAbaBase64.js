3cba428ae103f91e288fbc537e7ff511d172d8be
Cesar Goulart <devcesargoulart@gmail.com>
2022-03-30 22:32:20 -0300

    add getInfoFromJson function

--- src/extensions/getInfoAbaBase64.js
+++ src/extensions/getInfoAbaBase64.js
@@ -1,10 +1,10 @@
 // add your CLI-specific functionality here, which will then be accessible
 // to your commands
 module.exports = toolbox => {
-  toolbox.getInfoAbaBase64 = (abaBase64) => {
- 
-      let aba = JSON.parse(buf.toString(abaBase64))
-      return aba.config.cells
+  toolbox.getInfoAbaBase64 = ({ abaBase64,env_vars }) => {
+    let abaJson = buf.toString(abaBase64);
+    let abaObj = JSON.parse(abaJson);
+      return abaObj.config.cells
           .filter((node)=> Array.isArray(node.actions))
           .reduce((ret, node) => {
               ret.push(...node.actions
@@ -12,18 +12,9 @@ module.exports = toolbox => {
                   .filter(action=>action.name==="http_request"))
               return ret
           },[])
-          .map(node=>({
-            node: node.node, 
-            aba:aba.name, 
-            endpoint: node.parameters.config.url}))  
+          .map(node=>({node:node.node, aba:abaObj.name, endpoint: node.parameters.config.url}))  
 
   }
 
-  // enable this if you want to read configuration in from
-  // the current folder's package.json (in a "get-http-request-from-json" property),
-  // get-http-request-from-json.config.json, etc.
-  // toolbox.config = {
-  //   ...toolbox.config,
-  //   ...toolbox.config.loadConfig("get-http-request-from-json", process.cwd())
-  // }
+
 }
